024002567a87b52b936067329cf463a7e4413fa1
choniyuazwan <choniyuazwan@gmail.com>
2022-01-23 10:07:45 +0700

    initial

---
+++ src/main/java/id/choniyuazwan/keycloak/App.java
@@ -0,0 +1,11 @@
+package id.choniyuazwan.keycloak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!" );
+  }
+}
